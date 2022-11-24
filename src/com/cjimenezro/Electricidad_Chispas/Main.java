46d22f88c3b5730f3b31a1eebf3c7c7a973fa773
Cjr37 <103946097+CarlosAudiomeca@users.noreply.github.com>
2022-11-24 18:58:26 +0100

    Cambios en la clase main y creacion de clase productos y de interfaz ProductosServicios

--- src/com/cjimenezro/Electricidad_Chispas/Main.java
+++ src/com/cjimenezro/Electricidad_Chispas/Main.java
@@ -2,6 +2,28 @@ package com.cjimenezro.Electricidad_Chispas;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Autonomos autonomo = new Autonomos();
+        autonomo.getCodigoCliente();
+        autonomo.getCorporationName();
+        autonomo.getApellidos();
+        autonomo.getCorporationId();
+        autonomo.getEmail();
+        autonomo.getPoblacion();
+        autonomo.getProvincia();
+        autonomo.getTelefono();
+        autonomo.getDireccionPostal();
+
+
+        Sociedades sociedad = new Sociedades();
+        sociedad.getCodigoCliente();
+        sociedad.getCorporationName();
+        sociedad.getEmail();
+        sociedad.getPoblacion();
+        sociedad.getProvincia();
+        sociedad.getDireccionPostal();
+        sociedad.getTelefono();
+        sociedad.getCorporationId();
+
     }
 }
---
+++ src/com/cjimenezro/Electricidad_Chispas/Productos.java
@@ -0,0 +1,66 @@
+package com.cjimenezro.Electricidad_Chispas;
+
+public class Productos implements ProductosServicios{
+
+    private Integer codigo,precio,tipoIva;
+
+    private String nombre,marca,modelo;
+
+    //Funciones
+
+    @Override
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    @Override
+    public void setCodigo(Integer codigo) {
+        this.codigo=codigo;
+    }
+
+    @Override
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public void setNombre(String nombre) {
+        this.nombre=nombre;
+    }
+
+    @Override
+    public Integer getPrecio() {
+        return precio;
+    }
+
+    @Override
+    public void setPrecio(Integer precio) {
+        this.precio=precio;
+    }
+
+    @Override
+    public Integer getTipoIva() {
+        return tipoIva;
+    }
+
+    @Override
+    public void setTipoIva(Integer tipoIva) {
+        this.tipoIva=tipoIva;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+}
